6c7bf45bbefb25fe86479b573710872fb95e4ca1
Majennbi <85588366+Majennbi@users.noreply.github.com>
2023-01-27 17:43:30 +0100

    create dish and deal entities and use of fixtures

---
+++ src/Entity/Dish.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DishRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: DishRepository::class)]
+class Dish
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(min: 3, max: 50)]
+    private ?string $Category = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(min: 3, max: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    #[Assert\Positive()]
+    #[Assert\LessThan(value: 1000)]
+    #[Assert\NotNull()]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::BLOB, nullable: true)]
+    private $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(string $Category): self
+    {
+        $this->Category = $Category;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
